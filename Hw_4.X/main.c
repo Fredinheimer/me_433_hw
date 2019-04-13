19832558358219217208bf03e1fb3899dc80e347
Fredinheimer <michaelhendon96@gmail.com>
2019-04-13 13:38:53 -0500

    tested output (weird waves)

--- Hw_4.X/main.c
+++ Hw_4.X/main.c
@@ -1,4 +1,41 @@
 #include <math.h> 	//for sine wave plotting
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include <stdio.h>
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock x24 after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock by 2 to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 // Demonstrates spi by accessing external ram
 // PIC is the master, ram is the slave
 // Uses microchip 23K256 ram chip (see the data sheet for protocol details)
@@ -25,22 +62,21 @@ unsigned char spi_io(unsigned char o) {
 }
 
 // initialize spi4 and the ram module
-void spi_init() {
+void init_spi() {
   // set up the chip select pin as an output
   // the chip select pin is used by the sram to indicate
   // when a command is beginning (clear CS to low) and when it
   // is ending (set CS high)
   TRISBbits.TRISB8 = 0;
-  CS = 1;
 
   // Master - SPI4, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
   // we manually control SS4 as a digital output (F12)
   // since the pic is just starting, we know that spi is off. We rely on defaults here
  
-  // setup spi1 , all bits must be changed for SPI1 from SPI4
+  // setup spi1
   SPI1CON = 0;              // turn off the spi module and reset it
   SPI1BUF;                  // clear the rx buffer by reading from it
-  SPI1BRG = 0x?5DBF?;            // baud rate to 1 kHz [SPI4BRG = (48000000/(2*desired))-1]
+  SPI1BRG = 0x1;            // baud rate to 10 MHz [SPI4BRG = (48000000/(2*desired))-1]
   SPI1STATbits.SPIROV = 0;  // clear the overflow bit
   SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
   SPI1CONbits.MSTEN = 1;    // master operation
@@ -48,17 +84,20 @@ void spi_init() {
   RPB8Rbits.RPB8R = 0b0011; // sets SD01 to pin B8
   
                           
-  CS = 0;
 }
 
 
 void setVoltage(char channel, int voltage){
     unsigned char c1, c2;
-    unsigned char BUFGASHD = 0b111;
-    c1 = BUFGASHED << 4;
+    c1 = 0b111 << 4;
     c1 = c1 | (channel<<7);
     c1 = c1 | (voltage >> 6);
-    c2 = voltage & 0b0000 1111 1111;
+    c2 = voltage & 0b000011111111;
+    CS = 0;
+    spi_io(c1);
+    spi_io(c2);
+    CS = 1;
+    
     
 
 }
@@ -66,35 +105,36 @@ void setVoltage(char channel, int voltage){
 int main(void) {
 
   CS = 1;
+ __builtin_disable_interrupts();
 
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+    ANSELA = 0;
+    ANSELB = 0;
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
 	init_spi();
-
+    TRISBbits.TRISB7 = 0;
+     __builtin_enable_interrupts();
+    int i = 0;
   while(1) {
-	_CPO_SET_COUNT(0);
-	float f = 512 +512*sin(i*2*3.1415/1000*10);  //should make a 10Hz sin wave)
-	i++;
+    
+	int s = 512.0 +512.0*sin(i*2*3.1415/1000*10);  //should make a 10Hz sin wave)
+	int t = 512.0 +512/3.1415/0.2*asin(sin(3.1415*i));
+    i++;
 
 
 
-	setVoltage(0,512);		//test
-	setVoltage(1,256);		//test
-
-	while(_CPO_GET_COUNT() < 2400000000/1000) {}  //check this is 24Million
-    ;
-  }
+	setVoltage(0,t);		//send to DABb
+	setVoltage(1,s);		//send to DACa
+    _CP0_SET_COUNT(0);
+	while(_CP0_GET_COUNT() < 48000) {;}  //wait 1 khz
+    }
   return 0;
-}
-
-
-/*
-void setVoltage(char a, int v) {
-	unsigned short t = 0;
-	t= a << 15; //a is at the very end of the data transfer
-	t = t | 0b01110000000000000;
-	t = t | ((v&0b1111111111) <<2); //rejecting excessive bits (above 10)
-	
-	CS = 0;
-	spi_io(t>>8);
-	spi_
-	
 }
